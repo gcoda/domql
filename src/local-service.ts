a20a2bf0cf98772062f14307e5e790be11cb0718
gcoda <gcoding@gmail.com>
2020-02-11 20:55:41 +0200

    feat: basic resolvers

--- src/local-service.ts
+++ src/local-service.ts
@@ -1,18 +1,41 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
 import gql from 'graphql-tag'
+type NodeInternal = {
+  node?: HTMLElement
+}
 export const localService = {
   name: 'hello',
   typeDefs: gql`
     scalar DOMNode
     type Node {
       node: DOMNode
+      text: String
+      html: String
+      classList: [String]
+      attr(name: String!): String
+      select(query: String): Node
     }
     extend type Query {
       select(query: String): Node
     }
   `,
   resolvers: {
+    Node: {
+      attr: (root: null | NodeInternal, { name = '' }) =>
+        root?.node?.getAttribute(name),
+      text: (root: null | NodeInternal) => root?.node?.innerText,
+      html: (root: null | NodeInternal) => root?.node?.innerHTML,
+      classList: (root: null | NodeInternal) =>
+        root?.node?.getAttribute('class')?.split(' '),
+      select: (root: null | NodeInternal, { query = 'body' }) => {
+        const parent = root?.node || document
+        return {
+          node: parent.querySelector(query),
+        }
+      },
+    },
     Query: {
-      select: (_: object, { query = 'body' }) => {
+      select: (root: null, { query = 'body' }) => {
         return {
           node: document.querySelector(query),
         }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 const domServer = () =>
   import(/* webpackChunkName: "domServer" */ './dom-server')
+
 domServer()
   .then(exec => exec?.default)
   .then(exec => {
