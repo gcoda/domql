6812df180ab1760aabb0041d047a96ac19c6ae9a
gcoda <gcoding@gmail.com>
2020-02-15 13:17:20 +0200

    refactor: remove unused code

--- src/content-scripts/content-script.ts
+++ src/content-scripts/content-script.ts
@@ -9,23 +9,3 @@ browser.runtime.onMessage.addListener(async (message: any, sender: any) => {
   const dom = await domServer
   return dom(message.query)
 })
-
-/*
-const gql = String.raw
-domServerModule()
-  .then(exec => exec.default)
-  .then(domServer => {
-    domServer(gql`
-      query allLinks {
-        document {
-          title
-          links: selectAll(selector: "a") {
-            href: attr(name: "href")
-            text
-          }
-        }
-      }
-    `).then(({ data, errors }) => console.log(data))
-  })
-
-  */
