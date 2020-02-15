3ac85a419b7b5a850b96002a1fafe8df52d02824
gcoda <gcoding@gmail.com>
2020-02-15 02:09:48 +0200

    dev: gql module types

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '*.gql' {
+  import { DocumentNode } from 'graphql'
+  const content: DocumentNode
+  export default content
+}
