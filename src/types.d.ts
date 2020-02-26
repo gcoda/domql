9055d08c9a0f5e7b6931934b9f7ee515de3cd0d8
gcoda <gcoding@gmail.com>
2020-02-26 18:49:37 +0200

    chore: types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,47 @@
 declare module 'graphql-directive' {
-  import { GraphQLSchema } from 'graphql'
-
+  import { GraphQLSchema, GraphQLResolveInfo } from 'graphql'
+  type Args = { [arg: string]: undefined | any }
   type ResolveFn<S> = (args?: any) => S | Promise<S>
-  type DirectiveResolver = (
+  export type DirectiveResolver<TContext = any, TArgs extends Args = Args> = (
     resolve: ResolveFn<any>,
     source: any,
-    directiveArgs: any,
-    context: any,
-    info: any
+    directiveArgs: TArgs,
+    context: TContext,
+    info?: GraphQLResolveInfo
   ) => Promise<any>
-  export type DirectiveResolvers = { [k: string]: DirectiveResolver }
+  export type DirectiveResolvers<TContext = any> = {
+    [k: string]: DirectiveResolver<TContext>
+  }
   export const addDirectiveResolveFunctionsToSchema: (
     schema: GraphQLSchema,
     directives: DirectiveResolvers
   ) => void
+}
+
+declare module 'datastore-fs' {
+  import { Datastore, Key, Batch, Query, Result } from 'interface-datastore'
+  export default class MemoryDatastore<Value = Buffer>
+    implements Datastore<Value> {
+    constructor(path: string)
+    open(): Promise<void>
+    put(key: Key, val: Value): Promise<void>
+    get(key: Key): Promise<Value>
+    has(key: Key): Promise<boolean>
+    delete(key: Key): Promise<void>
+    batch(): Batch<Value>
+    query(q: Query<Value>): AsyncIterable<Result<Value>>
+    close(): Promise<void>
+  }
+}
+declare module 'jsonschema-formatter' {
+  export const validateSchema: (
+    p: any,
+    schema: any,
+    resCode: any
+  ) => Promise<any>
+}
+
+declare module 'prismjs' {
+  // @ts-ignore
+  export { default } from '@types/prismjs'
 }
