df0fab345f264e1d0f2138ad333c9e5bc600b82a
gcoda <gcoding@gmail.com>
2020-02-26 18:40:31 +0200

    chore: productionTip

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueCompositionApi from '@vue/composition-api'
 
+Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   render: h => h(App),
