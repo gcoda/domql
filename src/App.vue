0b75e8378c6d743d8954695123e7abbb187771c7
gcoda <gcoding@gmail.com>
2020-02-26 18:53:13 +0200

    feat: serve:web for developing as web page

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,96 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="h-full">
+    <editor class="h-full" />
+    <div class="hidden">
+      <a href="#test" title="I Am Title">Some link</a>
+      <a href="/about-page">About</a>
     </div>
-    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@import './assets/styles/tailwind.postcss';
+@import './assets/fonts.css';
+body,
+html {
+  @apply h-full;
 }
 </style>
+
+<script lang="ts">
+/*
+import { validateSchema } from 'jsonschema-formatter'
+validateSchema(
+  {
+    name: 'Barack Obama',
+    address: {
+      lines: ['1600 Pennsylvania Avenue Northwest'],
+      zip: 'DC 20500',
+      city: 'Washington',
+      country: '1',
+    },
+    votes: 0,
+  },
+  {
+    id: '/SimplePerson',
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      address: {
+        type: 'object',
+        properties: {
+          lines: {
+            type: 'array',
+            items: { type: 'string' },
+          },
+          zip: { type: 'string' },
+          city: { type: 'string' },
+          country: { type: 'string' },
+        },
+        required: ['country'],
+      },
+      votes: { type: 'integer', minimum: 1 },
+    },
+    required: ['name'],
+  },
+  {}
+)
+  .then(console.log)
+  .catch(console.log)
+/* */
+
+/* global globalThis:writable */
+import Editor from '@/components/Editor.vue'
+import { createComponent, ref } from '@vue/composition-api'
+export default createComponent({
+  components: { Editor },
+  setup() {
+    const defaultWidth = 480
+    const defaultHeight = 480
+    const height = ref(defaultHeight)
+    const width = ref(defaultWidth)
+    if (globalThis.browser)
+      browser?.windows.getCurrent().then(w => {
+        if (w.width && w.height) {
+          const halfWidth = w.width / 2
+
+          width.value =
+            halfWidth < defaultWidth
+              ? defaultWidth
+              : halfWidth > 720
+              ? 720
+              : halfWidth
+
+          height.value = w.height / 2
+          document
+            .querySelector('html')
+            ?.style.setProperty('--width', `${width.value}`)
+          document
+            .querySelector('html')
+            ?.style.setProperty('--height', `${height.value}`)
+        }
+      })
+    return { width, height }
+  },
+})
+</script>
