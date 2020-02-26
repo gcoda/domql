7e1f7bf36ee520fcccf8317516327337f59c127d
gcoda <gcoding@gmail.com>
2020-02-26 18:40:17 +0200

    feat: atmept at dynamic popup width

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,20 +1,52 @@
 <template>
-  <editor />
+  <div id="app" class="h-full">
+    <editor class="h-full" />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
 import Editor from '@/components/Editor.vue'
+import { createComponent, ref } from '@vue/composition-api'
 
-export default Vue.extend({
-  name: 'App',
+export default createComponent({
   components: { Editor },
+  setup() {
+    const defaultWidth = 480
+    const defaultHeight = 480
+    const height = ref(defaultHeight)
+    const width = ref(defaultWidth)
+    browser.windows.getCurrent().then(w => {
+      if (w.width && w.height) {
+        const halfWidth = w.width / 2
+
+        width.value =
+          halfWidth < defaultWidth
+            ? defaultWidth
+            : halfWidth > 720
+            ? 720
+            : halfWidth
+
+        height.value = w.height / 2
+        document
+          .querySelector('html')
+          ?.style.setProperty('--width', `${width.value}`)
+        document
+          .querySelector('html')
+          ?.style.setProperty('--height', `${height.value}`)
+      }
+    })
+    return { width, height }
+  },
 })
 </script>
 
 <style>
+@import '../assets/styles/tailwind.postcss';
+@import '../assets/fonts.css';
 html,
 body {
+  /* width: calc(var(--width, 480) * 1px);
+  height: calc(var(--height, 480) * 1px); */
   width: 480px;
   height: 480px;
   margin: 0;
