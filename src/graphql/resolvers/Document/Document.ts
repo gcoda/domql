4a674f864b3cc9c9d27ecd1716b321ee02846fdc
gcoda <gcoding@gmail.com>
2020-02-27 04:35:54 +0200

    feat: Mercury Parser

--- src/graphql/resolvers/Document/Document.ts
+++ src/graphql/resolvers/Document/Document.ts
@@ -1,10 +1,29 @@
 import { select, selectAll } from '@/graphql/resolvers/common/SelectResolver'
 import { echo } from '@/graphql/resolvers/common/Echo'
 import DocumentInterface from './DocumentInterface'
+const mercury = () =>
+  import(/* webpackChunkName: "Mercury" */ '@postlight/mercury-parser')
+const Mercury = (() => mercury().then(m => m.default || m))()
+// import Mercury from '@postlight/mercury-parser'
 export default {
   echo,
   title: root => root?.document?.title,
   select,
+  mercury: (root, { contentType = 'markdown' } = {}) => {
+    if (!root?.document) return null
+
+    return Mercury.then(mercury =>
+      mercury
+        .parse(document.location.href, {
+          contentType,
+          html: root.document?.documentElement.innerHTML,
+        })
+        .then(result => ({
+          __typename: 'MercuryParsed',
+          ...result,
+        }))
+    )
+  },
   selectAll,
   referrer: root => root?.document?.referrer,
   location: root => root?.document?.location?.href,
